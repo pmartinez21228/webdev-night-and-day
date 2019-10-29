e9eeff01ef3dbab821df25220afaf12fe8a075c4
Pablo Martinez <21228@columbushs.com>
2019-10-29 09:41:18 -0400

    Progress

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('button').click(function(){
+        if ($('body').hasClass('black')) {
+            $('body').removeClass('black').addClass('white');
+        }
+        else {
+            $('body').removeClass('white').addClass('black');
+        }
+    });
+});
+let changeColor = function(){
+    changeColor
+}
