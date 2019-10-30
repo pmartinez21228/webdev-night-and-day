63a4793141f981b8fd16e7e8ee28dc61e0fe3749
Pablo Martinez <pmartinez21228@columbushs.com>
2019-10-29 23:33:12 -0400

    Finally finished

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 $(document).ready(function() {
-    $('button').click(function(){
-        if ($('body').hasClass('black')) {
-            $('body').removeClass('black').addClass('white');
+    $('#orb').click( function() {        
+        if ($(this).hasClass('sun')) {
+            $(this).removeClass('sun').addClass('moon');
         }
         else {
-            $('body').removeClass('white').addClass('black');
+            $(this).removeClass('moon').addClass('sun');
+        }
+        if ($('#sky').hasClass('day')) {
+            $('#sky').removeClass('day').addClass('night');
+        }
+        else {
+            $('#sky').removeClass('night').addClass('day');
+        }
+        if ($('#moonspot1').hasClass('visible')) {
+            $('#moonspot1').removeClass('visible');
+        } else {
+            $('#moonspot1').addClass('visible');
+        }
+        if ($('#moonspot2').hasClass('visible')) {
+            $('#moonspot2').removeClass('visible');
+        } else {
+            $('#moonspot2').addClass('visible');
+        }
+        if ($('#moonspot3').hasClass('visible')) {
+            $('#moonspot3').removeClass('visible');
+        } else {
+            $('#moonspot3').addClass('visible');
         }
     });
 });
-let changeColor = function(){
-    changeColor
-}
